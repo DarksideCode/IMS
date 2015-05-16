8c375f162da8ab4468dcb2838921a0a4d5f1201c
Dominik Traise <Dominiktraise@gmx.de>
2015-05-16 18:42:41 +0200

    delete unnecessary files.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,7 +32,7 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
-                                array('label'=>'Registrieren', 'url'=>array('/site/registration')),
+                                array('label'=>'Registrieren', 'url'=>array('/site/registration'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
